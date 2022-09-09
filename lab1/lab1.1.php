e5bc6167521beb7d04e5b80b07ae0bca6b628aa1
ernesto-campos21 <ernesto.campos@utp.ac.pa>
2022-09-08 20:00:21 -0500

    lab1

---
+++ lab1/lab1.1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>laboratorio 1.1</title>
+</head>
+<body>
+    <?php
+        $ini = "hola";
+        $fin = " a todos";
+        $todo = $ini.$fin;
+        echo $todo;
+    ?>
+    
+</body>
+</html>
