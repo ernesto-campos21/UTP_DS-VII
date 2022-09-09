ed2c8b739336f8714e7ef3350638cddc1d3e4817
ernesto-campos21 <ernesto.campos@utp.ac.pa>
2022-09-08 20:03:44 -0500

    lab1.2

---
+++ lab1/lab1.2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>laboratorio 1.2</title>
+</head>
+<body>
+    <?php
+        $n1 = 1;
+        $n2 = 2;
+        $suma = $n1  + $n2;
+        echo "suma = ".$suma."<br>";
+        echo "$n1  + $n2";
+    ?>
+    
+</body>
+</html>
