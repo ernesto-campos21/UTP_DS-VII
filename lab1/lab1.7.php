2e576b94ab3c6c30510c40514e677e3333661feb
ernesto-campos21 <ernesto.campos@utp.ac.pa>
2022-09-11 23:46:32 -0500

    terminaci[on de laboratorios 1

---
+++ lab1/lab1.7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>laboratorio 1.7</title>
+</head>
+<body>
+    <?php
+        $x = 24;
+        $pi = 3.1416;
+        $animal = "conejo";
+        $saludo = "hola caracola";
+        echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo; 
+    ?>
+    
+</body>
+</html>
---
+++ lab1/lab1.8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>laboratorio 1.8</title>
+</head>
+<body>
+    <?php
+        $a = 8;
+        $b = 3;
+        echo $a + $b, "<br>";
+        echo $a - $b, "<br>";
+        echo $a * $b, "<br>";
+        echo $a / $b, "<br>";
+        $a++; echo $a, "<br>";
+        $b--; echo $b, "<br>";
+    ?>
+    
+</body>
+</html>
